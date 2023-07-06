d72c1d7a3cf99c0c92697fafa3e73cfd36a9a071
lamb0693 <mar189@naver.com>
2023-07-06 12:12:52 +0900

    2nd Test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+function ExampleComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log('Effect executed');
+
+    // Cleanup function
+    return () => {
+      console.log('Cleanup function executed');
+    };
+  }, [count]);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Header Area</h1>
       </header>
+
+      <ExampleComponent />
+
     </div>
   );
 }
