efea8bf74b84fd34d2a637bf1cc32e87355465e6
lamb0693 <mar189@naver.com>
2023-07-06 16:06:00 +0900

    CompleteDisp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,81 @@
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
 import './App.css';
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+const Index = () => {
+
+  const [ret, SetRet] = useState('');
+
+  useEffect( () => {
+    fetch("http://localhost:3001/towork")
+    .then( (res) => {
+      //console.log(res);
+      return res.json();
+    })
+    .then( (data) => {
+      //console.log(data);
+      SetRet( data.map( (xx) => {return ( <Link key={xx.id} to={`/work/${xx.id}`} ><span >{xx.id}</span></Link> )}) )
+    })
+    .catch( (err) => {
+      console.log(err);
+    })
+
+    return (
+      () => {   }
+    )
+
+  }, [])
+
+
+  return (
+    <div>
+      {ret}
+    </div>
+  )
+}
+
+const DispPage = () => {
+  const pageId = Number( useParams().id )
+  //console.log(pageId);
+
+  const [ret, SetRet] = useState('');
+
+  useEffect( () => {
+    fetch(`http://localhost:3001/towork/${pageId}`)
+    .then(
+      (res) => {
+        //console.log(res);
+        return res.json()
+      }
+    )
+    .then(
+      (data)=>{ 
+        SetRet( 
+          <div>
+             <span>id : {data.id}</span><span>title : {data.title}</span><span>detail : {data.detail}</span>
+          </div>
+        )
+      }
+    )
+    .catch(
+      (err) => {
+        console.log(err);
+      }
+    )
+      
+    return (
+      () => {   }
+    )
+
+  }, [pageId] )
+
+  return (
+    <div>{ret}</div>
+  )
+}
 
 function ExampleComponent() {
   const [count, setCount] = useState(0);
@@ -21,17 +97,32 @@ function ExampleComponent() {
   );
 }
 
+const Body = () => {
+  return (
+    <div>
+      <Index />
+      <DispPage />
+    </div>
+  )
+}
+
 function App() {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Header Area</h1>
-      </header>
+    <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <h1>Header Area</h1>
+          </header>
+          <Routes>
+            <Route path={"/"} element={<Index />} />
+            <Route path={"/video"} element={<ExampleComponent />} />
+            <Route path={"/work/:id"} element={<Body />} />
+          </Routes>
+        </div>
+    
+    </BrowserRouter>
 
-      <ExampleComponent />
-
-    </div>
   );
 }
 
