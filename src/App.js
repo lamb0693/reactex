accc8f4536017684168f0c9e43d24a65b23e1afd
lamb0693 <mar189@naver.com>
2023-07-07 15:48:56 +0900

    AddCreate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useParams, useNavigate } from 'react-router-dom';
 import './App.css';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
-
 
 const Index = () => {
 
@@ -31,9 +29,9 @@ const Index = () => {
 
 
   return (
-    <div>
+    <>
       {ret}
-    </div>
+    </>
   )
 }
 
@@ -73,7 +71,7 @@ const DispPage = () => {
   }, [pageId] )
 
   return (
-    <div>{ret}</div>
+    <>{ret}</>
   )
 }
 
@@ -106,7 +104,64 @@ const Body = () => {
   )
 }
 
+const Create = (props) => {
+  const navigate = useNavigate();
+
+  //console.log(props)
+  useEffect( () => {
+    props.setCreateModeOn()
+    console.log("setCreateModeOn executed")
+
+    return ()=>{
+      props.setCreateModeOff()
+      console.log("setCreateModeOff executed")
+    }
+  }, [props])
+
+
+  return (
+    <form onSubmit={ (event) => {
+        event.preventDefault()
+
+        const newData = {"id": event.target.id.value , "title": event.target.title.value, "detail":event.target.detail.value }
+
+        fetch(`http://localhost:3001/towork`, {
+          method: 'POST',
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify(newData)
+        })
+        .then(
+          (res) => {
+            //console.log(res);
+            return res.json()
+          }
+        )
+        .then(
+          (data)=>{ 
+  
+          }
+        )
+        .catch(
+          (err) => {
+            console.log(err);
+          }
+        )
+
+        navigate("/")
+      }}>
+      id : <input id="id" type="text" /> <br></br>
+      title : <input id="title" type="text" /> <br></br>
+      detail : <textarea id="detail" /><br></br>
+      <button>Create</button>
+    </form>
+  )
+}
+
 function App() {
+  const [onCreate, setOnCreate] = useState(false)
+
+  const setCreateModeOn = () => setOnCreate( true )
+  const setCreateModeOff = () => setOnCreate( false)
 
   return (
     <BrowserRouter>
@@ -118,7 +173,10 @@ function App() {
             <Route path={"/"} element={<Index />} />
             <Route path={"/video"} element={<ExampleComponent />} />
             <Route path={"/work/:id"} element={<Body />} />
+            <Route path={"/create"} element={<Create setCreateModeOn={setCreateModeOn}
+                 setCreateModeOff={setCreateModeOff} />} />
           </Routes>
+            {!onCreate && <div><Link to={"/create"} >Create New</Link></div>}
         </div>
     
     </BrowserRouter>
